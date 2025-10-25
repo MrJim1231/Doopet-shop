a26b47c9770ba96973b29efa5b05733013392aa3
Oleh <berolegnik@gmail.com>
2025-10-25 10:33:10 +0300

    делаем миксин

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,8 @@ import HeaderTopBar from "../layout/HeaderTopBar";
 import Header from "../layout/Header";
 import CatalogBlock from "../layout/CatalogBlock";
 import Breadcrumbs from "../layout/Breadcrumbs";
+import SectionHeader from "../components/SectionHeader";
+import graphicIcon from "../assets/icons/graphic-elements.svg";
 
 function About() {
   return (
@@ -10,6 +12,18 @@ function About() {
       <Header />
       <CatalogBlock />
       <Breadcrumbs items={[{ label: "О нас" }]} />
+
+      <section className="about">
+        <div className="container">
+          <SectionHeader
+            icon={graphicIcon}
+            title="О нас"
+            baseClass="about__header"
+          />
+
+          {/* Здесь можешь добавить основной контент страницы "О нас" */}
+        </div>
+      </section>
     </div>
   );
 }
